b19003383f95f58d42856aa0369a6f22163dde30
javierlnmn <jlm03123@gmail.com>
2023-07-09 15:18:38 +0200

    VideoComments funcionalidad completa

--- src/components/VideoComments.js
+++ src/components/VideoComments.js
@@ -7,7 +7,7 @@ import CommentItem from './CommentItem';
 const VideoComments = ({ video }) => {
 
     const videoId = video.id.videoId;
-    const [videoComments, setVideoComments] = useState([]);  
+    const [videoComments, setVideoComments] = useState([]);
     const [nextPageToken, setNextPageToken] = useState();
 
     const fetchVideoComments = async () => {
@@ -26,11 +26,11 @@ const VideoComments = ({ video }) => {
         );
 
         setNextPageToken(response.data.nextPageToken);
-        createComments(response.data.items);
+        addComments(response.data.items);
 
     }
 
-    const createComments = (comments) => {
+    const addComments = (comments) => {
         setVideoComments(prevComments => [...prevComments, ...comments.map((comment) => <CommentItem key={comment.id} comment={comment} />)]);
     }
 
@@ -39,14 +39,19 @@ const VideoComments = ({ video }) => {
     }
 
     useEffect(() => {
+        setVideoComments([]);
         fetchVideoComments();
     }, [video]);
 
     return (
         <div className='mt-3 pt-3 border-t-[1px] border-t-zinc-600'>
-            <h2 className='text-zinc-100 text-xl font-extrabold'>Comments</h2>
+            <h2 className='text-zinc-100 text-xl font-extrabold'>Comments ({video.statistics.commentCount})</h2>
             {videoComments}
-            <button className='transition-all shadow-md hover:shadow-2xl py-1 px-3 bg-teal-600 hover:bg-teal-500 text-zinc-100 rounded-md font-bold' onClick={seeMoreComments}>See more comments ({video.statistics.commentCount})</button>
+            { nextPageToken ?
+                <button className='transition-all shadow-md hover:shadow-2xl py-1 px-3 bg-teal-600 hover:bg-teal-500 text-zinc-100 rounded-md font-bold' onClick={seeMoreComments}>See more </button>
+                :
+                <p className='text-zinc-500 font-light'>There are no more comments</p>
+            }
         </div>
     );
 }
