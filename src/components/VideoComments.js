71f1e950ef015e141535d01d5f65283b297a826c
javierlnmn <jlm03123@gmail.com>
2023-07-12 09:52:30 +0200

    Correccion menor boton comentarios

--- src/components/VideoComments.js
+++ src/components/VideoComments.js
@@ -47,7 +47,7 @@ const VideoComments = ({ video }) => {
         <div className='mt-3 pt-3 border-t-[1px] border-t-zinc-600'>
             <h2 className='text-zinc-100 text-xl font-extrabold'>Comments ({video.statistics.commentCount})</h2>
             {videoComments}
-            { nextPageToken ?
+            { nextPageToken && video.statistics.commentCount > 20  ?
                 <button className='transition-all shadow-md hover:shadow-2xl py-1 px-3 bg-teal-600 hover:bg-teal-500 text-zinc-100 rounded-md font-bold' onClick={seeMoreComments}>See more </button>
                 :
                 <p className='text-zinc-500 font-light'>There are no more comments</p>
